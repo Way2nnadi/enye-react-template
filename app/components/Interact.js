f373d53673ee3b86fa11adf2b4f2ed55585a679f
Way2nnadi <ucheunnadi@gmail.com>
2018-06-07 08:44:22 -0700

    [fix] className

--- app/components/Interact.js
+++ app/components/Interact.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { EXAMPLE_APP_MESSAGE } from './constant';
 import './Interact.css';
 
 class Interact extends React.Component {
   render() {
     return (
       <div>
-        <div class="header">
-          <div class="container">
+        <div className="header">
+          <div className="container">
             <h1>LETS INTERACT STITCHES</h1>
               <p>Connect With Designers All Over Nigeria</p>
-              <a class="proceed" href="#">Log in</a>
+              <a className="proceed" href="#">Log in</a>
           </div>
         </div>
 
-        <div class="nav">
-          <div class="container">
+        <div className="nav">
+          <div className="container">
             <ul>
               <li><a href="#register">Register</a></li>
               <li><a href="#trainings">Trainings</a></li>
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require('serve-static');
 const router = require('./router.js');
 
 const server = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 
 server.use(serveStatic('build', { index: false }));
